2b7c4055df6ae3b7dc917d4a5d5d49d7ff6bdad1
root <yoazmenda@gmail.com>
2024-03-06 15:14:44 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/_health', methods=['GET'])
+def generate():
+    return "healty and running"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
